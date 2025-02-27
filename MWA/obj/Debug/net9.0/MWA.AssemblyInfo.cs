df23240d64e727608eed0c0e50bc0e3752a69934
Belal <belalafzal19@gmail.com>
2025-02-27 22:42:00 +0530

    Commited

--- MWA/obj/Debug/net9.0/MWA.AssemblyInfo.cs
+++ MWA/obj/Debug/net9.0/MWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf63241535788555de7282cca3495ac699a8554")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be3a105352dee6a858aba81a2041418e6641a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
