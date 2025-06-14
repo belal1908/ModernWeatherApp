4664a2c1dd0eb22a6c3145fad090ff18828252d2
Belal <belalafzal19@gmail.com>
2025-06-14 11:08:39 +0530

    Commited

--- MWA/Models/UserAlertPreference.cs
+++ MWA/Models/UserAlertPreference.cs
@@ -6,5 +6,7 @@ namespace MWA.Models
         public string UserId { get; set; }
         public double TempThreshold { get; set; } = 35;
         public double WindThreshold { get; set; } = 25;
+
+        public bool EmailAlertsEnabled { get; set; } = true;
     }
 }
